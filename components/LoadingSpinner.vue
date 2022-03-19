f3c422438984efd71206d4ac524299af2eddb8ef
Olivier Colas <olivercolas.oc@gmail.com>
2022-03-19 12:11:43 +0100

    feat: added loading spinner component

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-else class="d-flex align-items justify-center content-container">
+    <v-progress-circular indeterminate></v-progress-circular>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class LoadingSpinner extends Vue {
+}
+</script>
